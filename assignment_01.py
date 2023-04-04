b4db2e34e6adb5aec43046b33147015626f6f47f
Vibha Chawda <vibhachawda@gmail.com>
2023-04-03 22:35:29 -0400

    Python_files_1

---
+++ assignment_01.py
@@ -0,0 +1,55 @@
+# Assignment 1:
+"""
+    We would like to get the remainder of 15 divided by 4.
+    The calculation below does not seem to give us this result.
+    How would you change the code to meet the requirement?
+
+"""
+num1 = 15
+num2 = 4
+
+answer = num1 % num2
+print(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution:
+
+# remainder = 15 % 4
+# print(remainder)
+
